26c1a5d540a81579c1e6321d6ee820548d66444a
coLATin <colajob2008@gmail.com>
2020-10-29 23:01:59 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -43,4 +43,5 @@ sed -i 's/"带宽监控"/"监控"/g' feeds/luci/applications/luci-app-nlbwmon/po
 sed -i 's/"UPnP"/"UPnP管理"/g' feeds/luci/applications/luci-app-upnp/po/zh-cn/upnp.po
 sed -i 's/"京东签到服务"/"领取京豆插件"/g' feeds/jerryk/luci-app-jd-dailybonus/po/zh-cn/jd-dailybonus.po
 sed -i 's/"PassWall"/"PassWall科学上网"/g' feeds/xiaorouji/lienol/luci-app-passwall/po/zh-cn/passwall.po
+sed -i 's/"ZeroTier"/"ZeroTier内网穿透"/g' package/lean/luci-app-zerotier/po/zh-cn/zerotier.po
 # sed -i 's/"ShadowSocksR Plus+"/"SSR-Plus科学上网"/g' feeds/jerryk/luci-app-jd-dailybonus/po/zh-cn/jd-dailybonus.po
