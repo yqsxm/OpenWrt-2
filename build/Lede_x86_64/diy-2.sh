a9c5d695f9879ae0175b350fe7baf627e0afcc25
coLATin <colajob2008@gmail.com>
2020-10-29 22:06:29 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -33,10 +33,14 @@ sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftp
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
-sed -i 's/"KMS 服务器"/"KMS激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
+sed -i 's/"KMS 服务器"/"微软产品激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 sed -i 's/"TTYD 终端"/"命令窗"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
-sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
+sed -i 's/"USB 打印服务器"/"打印机共享"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+sed -i 's/"管理权"/"修改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/"带宽监控"/"监控"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+sed -i 's/"UPnP"/"UPnP管理"/g' feeds/luci/applications/luci-app-upnp/po/zh-cn/upnp.po
+sed -i 's/"京东签到服务"/"领取京豆插件"/g' feeds/jerryk/luci-app-jd-dailybonus/po/zh-cn/jd-dailybonus.po
+sed -i 's/"PassWall"/"PassWall科学上网"/g' feeds/xiaorouji/lienol/luci-app-passwall/po/zh-cn/passwall.po
+# sed -i 's/"ShadowSocksR Plus+"/"SSR-Plus科学上网"/g' feeds/jerryk/luci-app-jd-dailybonus/po/zh-cn/jd-dailybonus.po
