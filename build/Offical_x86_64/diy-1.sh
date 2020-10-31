d0122455889338a8f99185d1cb76226dfd25b95c
coLATin <colajob2008@gmail.com>
2020-10-31 23:27:23 +0800

    Update diy-1.sh

--- build/Offical_x86_64/diy-1.sh
+++ build/Offical_x86_64/diy-1.sh
@@ -3,7 +3,8 @@
 #
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
-
+rm -rf package/lienol/luci-app-timecontrol
+rm -rf package/ctcgfw/luci-theme-argonv3
 
 #源码里面几乎囊括了所有插件，所以基本不需要添加插件了
 
